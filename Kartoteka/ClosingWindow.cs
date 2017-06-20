87463aecbcf725690024bcbef4ba63959599cbfa
Dsamofalov <09demon@rambler.ru>
2017-06-20 23:43:10 +0300

    /закрывание окон

---
+++ Kartoteka/ClosingWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kartoteka
+{
+   public static class ClosingWindow
+    {
+         public static void CloseWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+    }
+}
--- Kartoteka/ViewModel/MainViewModel.cs
+++ Kartoteka/ViewModel/MainViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using Kartoteka.Model;
+using System.Windows;
 using System.Windows.Input;
+using System;
 
 namespace Kartoteka.ViewModel
 {
@@ -12,48 +15,17 @@ namespace Kartoteka.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-       
+        public RelayCommand<Window> CloseWindowCommand { get; private set; }
         public ICommand AddNewAuthor { get; set; }
         public ICommand AddNewBook { get; set; }
         public ICommand StartNewSearch { get; set; }
-        public ICommand Exit { get; set; }
-        private readonly IDataService _dataService;
-
-        /// <summary>
-        /// The <see cref="WelcomeTitle" /> property's name.
-        /// </summary>
-        public const string WelcomeTitlePropertyName = "WelcomeTitle";
-
-        private string _welcomeTitle = string.Empty;
-
-        /// <summary>
-        /// Gets the WelcomeTitle property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public string WelcomeTitle
-        {
-            get
-            {
-                return _welcomeTitle;
-            }
-            set
-            {
-                Set(ref _welcomeTitle, value);
-            }
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
 
         public MainViewModel(IDataService dataService)
         {
             AddNewAuthor = new CommandAddAuthor();
             AddNewBook = new CommandAddBook();
             StartNewSearch = new CommandToSearch();
-            Exit = new CommandExit();
-
-      
+            this.CloseWindowCommand = new RelayCommand<Window>(ClosingWindow.CloseWindow);
         }
         
         ////public override void Cleanup()
